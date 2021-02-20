875bcf5e769878171ee9d3cd79afc47a5a400f1c
Quentin Hermiteau <quentinhermiteau@gmail.com>
2021-02-20 19:08:58 +0100

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "react-hooks"
+    ],
+    "rules": {
+        "react-hooks/rules-of-hooks": "error", // Vérifie les règles des Hooks
+        "react-hooks/exhaustive-deps": "warn"  // Vérifie les tableaux de dépendances
+    }
+};
