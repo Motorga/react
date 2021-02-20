7bd91040f720aef78c7e62021af87e8ee17bfa9e
Quentin Hermiteau <quentinhermiteau@gmail.com>
2021-02-20 19:21:47 +0100

    remove console log + add alt on img

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,11 +17,10 @@ const Header = () => {
         history.push('/login');
     }, [setToken, setUser, history]);
 
-    console.log(process.env.PUBLIC_URL)
     return (
         <Navbar className="shadow mb-2" collapseOnSelect expand="md" bg="light" variant="light" sticky="top">
             <Navbar.Brand>
-                <img className="mr-2" src={`${process.env.PUBLIC_URL}/icon.svg`} width="50" height="50"></img>
+                <img className="mr-2" src={`${process.env.PUBLIC_URL}/icon.svg`} alt="logo" width="50" height="50"></img>
                 Motorga
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
